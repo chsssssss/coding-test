7025a41cbffebd6f75b946aaaee481f9fe24e4a9
chsssssss <96712947+chsssssss@users.noreply.github.com>
2025-05-12 17:29:34 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,7 @@
+T = int(input())
+fibo = [(1, 0), (0, 1)]
+for i in range(2, 41):
+    fibo.append((fibo[i-2][0] + fibo[i-1][0], fibo[i-2][1] + fibo[i-1][1]))
+for _ in range(T):
+    N = int(input())
+    print(fibo[N][0], fibo[N][1])
