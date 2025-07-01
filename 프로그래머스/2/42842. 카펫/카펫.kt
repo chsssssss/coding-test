35ba9bea8c21773de11fd2b5f3f36c5861775b31
chsssssss <96712947+chsssssss@users.noreply.github.com>
2025-07-01 18:48:28 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(brown: Int, yellow: Int): IntArray {
+        var answer = intArrayOf()
+        var total = brown + yellow
+        for (height in 3..total) {
+            if (total % height == 0) {
+                val width = total / height
+                
+                if ((width - 2) * (height - 2) == yellow) {
+                    return intArrayOf(width, height)
+                }
+            }
+        }
+        return intArrayOf()
+    }
+}
