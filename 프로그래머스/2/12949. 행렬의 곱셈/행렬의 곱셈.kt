1ffefb6e625904721b1aac01ded1ab9e585cf66c
chsssssss <96712947+chsssssss@users.noreply.github.com>
2025-07-02 19:34:23 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.58 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        val answer = Array(arr1.size) { IntArray(arr2[0].size) }
+
+        for (i in arr1.indices) {
+            var result = ArrayList<Int>()
+            for (j in arr2[0].indices) {
+                var sum = 0
+                for (k in arr1[0].indices) {
+                    sum += arr1[i][k] * arr2[k][j]
+                    
+                }
+                answer[i][j] = sum
+            }
+            
+        }
+        return answer
+    }
+}
