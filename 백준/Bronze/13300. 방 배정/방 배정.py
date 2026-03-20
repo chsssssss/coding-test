8fc3636a527405deccf1d8ad835f08f52e4c0269
chsssssss <96712947+chsssssss@users.noreply.github.com>
2026-03-20 17:08:41 +0900

    [Bronze II] Title: 방 배정, Time: 32 ms, Memory: 32412 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,17 @@
+N, K = map(int, input().split())
+stu = {}
+result = 0
+for _ in range(N):
+    S, Y = map(int, input().split())
+    if (S, Y) in stu:
+        stu[(S, Y)] += 1
+    else:
+        stu[(S, Y)] = 1
+
+for key, val in stu.items():
+    if val > K:
+        result += val // K + 1
+    else:
+        result += 1
+
+print(result)
