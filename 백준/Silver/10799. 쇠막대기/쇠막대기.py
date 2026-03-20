e942669e1d96c162a2b15d59efd5d11b90002704
chsssssss <96712947+chsssssss@users.noreply.github.com>
2026-03-20 23:29:23 +0900

    [Silver II] Title: 쇠막대기, Time: 56 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,18 @@
+s = input()
+stack = []
+count = 0
+
+for i in range(len(s)):
+    if s[i] == '(':
+        stack.append('(')
+    else:
+        stack.pop()
+
+        if s[i-1] == '(':
+            # 레이저
+            count += len(stack)
+        else:
+            # 막대기 끝
+            count += 1
+
+print(count)
