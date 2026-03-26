b9b7daa50d423e4c58f43bdc529646921eb73988
chsssssss <96712947+chsssssss@users.noreply.github.com>
2026-03-26 11:34:01 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,21 +1,22 @@
 from collections import deque
+
 N, M = map(int, input().split())
-graph = [list(map(int, input().strip())) for _ in range(N)]
-visited = [[False] * (M+1) for _ in range(N)]
+graph = [list(map(int, input())) for _ in range(N)]
+queue = deque([(0, 0)])
 
-directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-    while queue:
-        x, y = queue.popleft()
-        for dx, dy in directions:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < N and 0 <= ny < M:
-                if graph[nx][ny] == 1:
-                    graph[nx][ny] = graph[x][y] + 1
-                    queue.append((nx, ny))
-bfs(0, 0)
+while queue:
+    x, y = queue.popleft()
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < N and 0 <= ny < M:
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
 print(graph[N-1][M-1])
