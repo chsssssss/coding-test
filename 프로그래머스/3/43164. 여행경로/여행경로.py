f538bc1dc0c18b9e21e2121a926ae35bd4e5f642
chsssssss <96712947+chsssssss@users.noreply.github.com>
2026-04-02 19:11:22 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+def solution(tickets):
+    answer = 0
+    graph = defaultdict(list)
+    for start, end in tickets:
+        graph[start].append(end)
+
+    for key in graph:
+        graph[key].sort(reverse=True)
+        
+    stack = ["ICN"]
+    path = []
+    while stack:
+        top = stack[-1]
+        
+        if top in graph and graph[top]:
+            stack.append(graph[top].pop())
+        else:
+            path.append(stack.pop())
+    return path[::-1]
