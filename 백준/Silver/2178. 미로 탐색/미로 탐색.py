0415e958f57035c4d70cb8f049e4314bfc606cbf
chsssssss <96712947+chsssssss@users.noreply.github.com>
2026-04-04 13:07:23 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,22 +1,16 @@
 from collections import deque
-
 N, M = map(int, input().split())
 graph = [list(map(int, input())) for _ in range(N)]
 queue = deque([(0, 0)])
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+visited = [[False] * M for _ in range(N)]
 
 while queue:
     x, y = queue.popleft()
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-
-        if 0 <= nx < N and 0 <= ny < M:
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-
+    for dx, dy in directions:
+        nx, ny = dx + x, dy + y
+        if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and graph[nx][ny] == 1:
+            graph[nx][ny] = graph[x][y] + 1
+            queue.append((nx, ny))
 print(graph[N-1][M-1])
